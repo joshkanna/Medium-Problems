a709c8f6f975721403a85e90a9d1d4c474e4292e
Joshua <joshuakannappilly10@gmail.com>
2024-02-01 16:21:02 +0000

    Refactored code to get expected value

--- join_student_club.rb
+++ join_student_club.rb
@@ -1,18 +1,34 @@
 def simualate_game(stop_condition)
   rolls = 0
+  first = Random.rand(1..6)
+  rolls += 1
 
   loop do
-    first = Random.rand(1..6)
-    rolls += 1
-    next unless first == 5
+    if first == 5
+      second = Random.rand(1..6)
+      rolls += 1
+      break if [first, second] == stop_condition
 
-    second = Random.rand(1..6)
-    rolls += 1
-    break if [first, second] == stop_condition
+      first = second
+    else
+      first = Random.rand(1..6)
+      rolls += 1
+    end
   end
 
   rolls
 end
 
-p simualate_game([5, 6])
-p simualate_game([5, 5])
+def expected_value(game_one, game_two, runs)
+  game_one_count = 0
+  game_two_count = 0
+  runs.times do
+    game_one_count += simualate_game(game_one)
+    game_two_count += simualate_game(game_two)
+  end
+
+  p "Game one average runs: #{game_one_count.to_f / runs}"
+  p "Game two average runs: #{game_two_count.to_f / runs}"
+end
+
+expected_value([5, 6], [5, 5], 100_000)
